a0c9ebf58f2d046bb1bcc87deb18c4de2520190c
sanvi yadav <sanvi.25b01010431.abes.ac.in>
2026-02-24 23:37:22 +0530

    operators

---
+++ assignment1.js
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <script type="text/javascript">
+        var a=100;var b=13;var linebreak="<br>";
+        document.write("a+b+c="");
+        result=a+b+c;
+        document.write(result);
+        document.write(linebreak);
+        document.write("a+b=");
+        result=a+b;
+        document.write(result);
+        document.write(linebreak);
+        document.write("a-b=");
+        result=a-b;
+        document.write(result);
+        document.write(linebreak);
+        document.write("a/b=");
+        result=a/b;
+        document.write(result);
+        document.write(linebreak);
+        document.write("a%b=");
+        result=a%b;
+        document.write(result);
+        document.write(linebreak);
+        </script>
+    </head>
+</html>
--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 console.log("Hello, world!");
 document.write("hello world");
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+var sayHello=function(){    //function in a variable; type is function
+    alert("hello world");
+}
+var numarray=[1,2,3,4,5];
+var animals=new Array("dog","cat","mouse");   //dynamic array
+var person={    //object   person.age is used to access the age property of the person object
+    name:"Sanvi",
+    age:19,
+}
+const sym1=Symbol(4)
+console.log(sym1);
+const sym2=Symbol(4);
+console.log(sym2);
+if(sym1==sym2){console.log("true");}
+else{console.log("false");}
+
