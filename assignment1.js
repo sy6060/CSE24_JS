3b8f7930ac96c4681d4bc5043c8d0bab4f4edf3d
sanvi yadav <sanvi.25b01010431.abes.ac.in>
2026-02-25 00:04:43 +0530

    assignment operators

--- assignment1.js
+++ assignment1.js
@@ -1,8 +1,8 @@
 <html>
     <head>
         <script type="text/javascript">
-        var a=100;var b=13;var linebreak="<br>";
-        document.write("a+b+c="");
+        var a=100; var b=13; var c =10; var linebreak = "\n";
+        document.write("a+b+c=");
         result=a+b+c;
         document.write(result);
         document.write(linebreak);
---
+++ assignment2.js
@@ -0,0 +1,29 @@
+<html>
+    <body>
+        <script type="text/javascript">
+        var a=10;var b=20; var linebreak="<br/>";
+        document.write("(a==b)=&gt;");
+        result=(a==b);
+        document.write(result);
+        document.write(linebreak);
+        document.write("(a&lt;b)=&gt;");
+        result=(a<b);
+        document.write(result);
+        document.write(linebreak);
+        document.write("(a&gt;b)=&gt;");
+        result=(a>b);
+        document.write(result);
+        document.write(linebreak);
+        document.write("(a!=b)=&gt;");
+        result=(a!=b);
+        document.write(result);
+        document.write(linebreak);
+        document.write("(a&lt;=b)=&gt;");
+        result=(a<=b);
+        document.write(result);
+        document.write(linebreak);
+        </script>
+
+        Set the variable a to different values and different operators and then try...
+    </body>
+</html>
